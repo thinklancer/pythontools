62e7bfc3d735a4f030fed0a7677b6f005b386ffd
thinklancer <thinklancer@gmail.com>
2012-12-07 20:47:22 +0100

    add input feature
    further:
    1. report process
    2. add formation (optional input)
    3. auto search file

--- getcomic.py
+++ getcomic.py
@@ -1,12 +1,20 @@
+#!/opt/local/bin/python
+
 # download comic by specify the first and last pages
+# individual file could be download by >curl
 import urllib
+import sys
+import numpy as np
+from subprocess import call
 
 def getcomic(url,begin,end):
     id = range(begin,end)
     for i in id:
-        web = url+'{0:{fill}3}'.format(i,fill='0')+'.jpg'
-        out = '{0:{fill}3}'.format(i,fill='0')+'.jpg'
+        web = url+'{0:{fill}2}'.format(i,fill='0')+'.jpg'
+        out = '{0:{fill}2}'.format(i,fill='0')+'.jpg'
         urllib.urlretrieve(web,out)
 
 if __name__ == '__main__':
-    getcomic('http://imgfast.manhua.178.com/i/Is/Vol_01/',1,97)
+    getcomic(sys.argv[1],np.int_(sys.argv[2]),np.int_(sys.argv[3])+1)
+    command = 'zip '+sys.argv[4]+' *.jpg'
+    call(command,shell=True)
